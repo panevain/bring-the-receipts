c85336508e530014457599e296ba6d6642227f23
Matt Newcomer <matt@panevain.dev>
2025-07-10 21:26:48 -0800

    v1 interface

---
+++ client/index.js
@@ -0,0 +1,51 @@
+const feedback = document.getElementById('feedback')
+const form = document.getElementById('form')
+
+function clearErrors() {
+    feedback.removeAttribute('class');
+}
+
+function hideFeedback(){
+    feedback.classList.add('hidden')
+    feedback.classList.remove('visible')
+}
+
+function showFeedback(){
+    feedback.classList.remove('hidden')
+    feedback.classList.add('visible')
+}
+
+form.addEventListener('input', () => {
+    clearErrors()
+    hideFeedback()
+})
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    const form = event.target;
+    const formData = new FormData(form);
+
+    clearErrors()
+    showFeedback()
+
+
+    fetch(form.action, {
+        method: form.method,
+        body: formData
+    })
+        .then(response => {
+            if (response.ok) {
+                form.reset();
+                feedback.classList.add('feedback-success')
+                feedback.innerText = "Success!"
+            } else {
+                feedback.classList.add('feedback-error')
+                feedback.innerText = "Failed to submit form!"
+            }
+        })
+        .catch(error => {
+            feedback.classList.add('feedback-error')
+            feedback.innerText = "Network error."
+        });
+})
