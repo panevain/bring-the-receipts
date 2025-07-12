3c52c4256a50ec4c6694df5fda983fa017246904
Matt Newcomer <matt@panevain.dev>
2025-07-12 07:54:56 -0800

    Styling updates and character counter

--- client/index.js
+++ client/index.js
@@ -1,5 +1,13 @@
 const feedback = document.getElementById('feedback')
 const form = document.getElementById('form')
+const textarea = document.getElementById('message-input')
+const lengthFeedback = document.getElementById('message-length-feedback');
+const maxLength = textarea.getAttribute('maxlength');
+
+function updateLengthFeedback() {
+    const currentLength = textarea.value.length;
+    lengthFeedback.textContent = `${currentLength}/${maxLength}`;
+}
 
 function clearErrors() {
     feedback.removeAttribute('class');
@@ -13,6 +21,10 @@ function hideFeedback() {
 function showFeedback() {
     feedback.classList.remove('hidden')
     feedback.classList.add('visible')
+}
+
+function setup() {
+    updateLengthFeedback()
 }
 
 form.addEventListener('input', () => {
@@ -44,8 +56,14 @@ form.addEventListener('submit', function (event) {
         })
         .catch(error => {
             feedback.classList.add('feedback-error')
-            feedback.innerText = "Network error."
+            feedback.textContent = "Network error."
         }).finally(() => {
             showFeedback()
         })
 })
+
+textarea.addEventListener('input', () => {
+    updateLengthFeedback()
+})
+
+setup()
